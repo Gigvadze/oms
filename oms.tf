d46b4423e807347b37a8f4231580e5cc17555480
henadiy <henadiy.kondratiuk@gmail.com>
2019-05-06 18:20:44 +0300

    added terraform file

--- oms.tf
+++ oms.tf
@@ -89,3 +89,6 @@ resource "aws_security_group" "nginx-sg" {
 output "aws_instance_public_dns" {
 	value = "${aws_instance.web.public_dns}"
 }
+output "aws_instance_public_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
